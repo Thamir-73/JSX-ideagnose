d19de7ff122a1a421dab19ea359852e3eb3732fe
Thamir-73 <Thamir.299@hotmail.com>
2024-08-22 12:40:56 +0300

    sdg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'ideagnose.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.ideagnose.com',
+        },
+      ],
+    },
+    // other configurations...
+  };
+  
+  export default nextConfig;
